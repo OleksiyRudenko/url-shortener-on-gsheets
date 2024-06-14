eda8044d94a389059c152ab849c2dabacef4ef71
Oleksiy Rudenko <8686631+OleksiyRudenko@users.noreply.github.com>
2024-06-14 19:26:00 +0200

    Update postdeploy.sh: fix project url

--- postdeploy.sh
+++ postdeploy.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 echo
 echo "$(tput setaf 1)IMPORTANT:$(tput sgr0)"
 echo "To finish the setup, go to"
-echo "$(tput smul)https://github.com/ahmetb/serverless-url-shortener$(tput sgr0)"
+echo "$(tput smul)https://github.com/OleksiyRudenko/url-shortener-on-gsheets$(tput sgr0)"
 echo "and follow the rest of the instructions."
